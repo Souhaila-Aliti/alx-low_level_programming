cdf13290e76083256e59b85424d60ba530b3c214
Souhaila-Aliti <Souhailaalitii@gmail.com>
2023-06-15 14:06:13 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - You are not allowed to use print
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
